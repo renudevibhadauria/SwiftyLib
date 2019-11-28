1517861d2e4237667796d8c126a0707458cfe83c
Renu Devi <renu.devi@publicissapient.com>
2019-11-28 12:40:52 +0530

    commit userstate

--- SwiftyLib/SwiftyLib.h
+++ SwiftyLib/SwiftyLib.h
@@ -16,4 +16,3 @@ FOUNDATION_EXPORT const unsigned char SwiftyLibVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SwiftyLib/PublicHeader.h>
 
-
